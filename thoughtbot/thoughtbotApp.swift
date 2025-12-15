570d6e253b1bddac88031f8182a72c15c427809d
Luka Eric <102396428+Lukaeric14@users.noreply.github.com>
2025-12-15 10:08:34 +0400

    Initial Commit

---
+++ thoughtbot/thoughtbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  thoughtbotApp.swift
+//  thoughtbot
+//
+//  Created by Luka Eric on 15/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct thoughtbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ thoughtbotTests/thoughtbotTests.swift
@@ -0,0 +1,17 @@
+//
+//  thoughtbotTests.swift
+//  thoughtbotTests
+//
+//  Created by Luka Eric on 15/12/2025.
+//
+
+import Testing
+@testable import thoughtbot
+
+struct thoughtbotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
